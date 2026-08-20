e920aff5bae51b4cde084b628fe24a1f059a78fa
Zach Manson <zachpmanson@gmail.com>
2026-08-20 10:07:21 +1000

    nix(fluux): bump npmDepsHash for the markdown-it dep
    apps/fluux now depends directly on markdown-it, which moves the lock
    (npmDepsHash). Regenerated with:
        nix run nixpkgs#prefetch-npm-deps -- package-lock.json

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
 
           # Regenerate after any package-lock.json change with:
           #   nix run nixpkgs#prefetch-npm-deps -- package-lock.json
-          npmDepsHash = "sha256-sysPd9spa+CTNOdHTFQDjDXEkKaAUcSrYsQ16alk0uw=";
+          npmDepsHash = "sha256-5uWTmi0wm6lR0Lj2CiozgoUeSrYfDYKowSYxFgPSXQo=";
 
           nodejs = pkgs.nodejs_24;
 
